79dc107cc165a764e81da632e42928453b9c142a
joeylong <1173286732@qq.com>
2018-03-19 23:39:05 +0800

    Initial commit

---
+++ src/main/java/com/eureka/producer/controller/EurekaProducerController.java
@@ -0,0 +1,21 @@
+package com.eureka.producer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EurekaProducerController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/producer")
+    public String testproducer(){
+        String services = "service:"+discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+
+}
